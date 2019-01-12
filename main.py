c5b20dd35d29336d944e9d044db52e08539f4ff8
Nicholas Ionata <n.ionata129@gmail.com>
2019-01-11 20:31:02 -0500

    Added DB config

--- main.py
+++ main.py
@@ -1,21 +1,19 @@
 from app import app
+from db_config import mysql
 from flask import jsonify
 from flask import flash, request
 
-@app.route('/hello')
-def hello():
-	try:
-		return "Hello World"
-	except Exception as e:
-		print(e)
-	finally:
-		print("no")
-
-@app.route('/event', methods=['POST'])
+@app.route('/api/event', methods=['POST'])
 def add_event():
 	try:
 		_json = request.json
+		print(_json)
 		_name = _json['name']
+		_type = _json['type']
+		_start = _start['start']
+		_end = _end['end']
+		_lat = _lat['lat']
+		_long = _long['long']
 		resp = jsonify('Event added successfully ' + _name)
 		resp.status_code = 200
 		return resp
