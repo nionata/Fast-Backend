1b60f895aec756c529399f46d1e0bc620832b6eb
Nicholas Ionata <n.ionata129@gmail.com>
2019-01-13 11:14:18 -0500

    Added api for new events

--- db_config.py
+++ db_config.py
@@ -4,7 +4,7 @@ from flaskext.mysql import MySQL
 mysql = MySQL()
 
 # MySQL configurations
-app.config('MYSQL_DATABASE_USER') = root
+app.config['MYSQL_DATABASE_USER'] = 'root'
 app.config['MYSQL_DATABASE_PASSWORD'] = 'password'
 app.config['MYSQL_DATABASE_DB'] = 'fast'
 app.config['MYSQL_DATABASE_HOST'] = 'localhost'
--- main.py
+++ main.py
@@ -1,3 +1,4 @@
+import pymysql
 from app import app
 from db_config import mysql
 from flask import jsonify
@@ -7,20 +8,34 @@ from flask import flash, request
 def add_event():
 	try:
 		_json = request.json
-		print(_json)
 		_name = _json['name']
 		_type = _json['type']
-		_start = _start['start']
-		_end = _end['end']
-		_lat = _lat['lat']
-		_long = _long['long']
-		resp = jsonify('Event added successfully ' + _name)
-		resp.status_code = 200
-		return resp
+		_start = _json['start']
+		_end = _json['end']
+		_lat = _json['lat']
+		_long = _json['long']
+		if True: #Add some logic
+			sql = "INSERT INTO events(event_name, event_type_id, event_start, event_end, event_lat, event_long) VALUES(%s, %s, %s, %s, %s, %s)"
+			data = (_name, _type, _start, _end, _lat, _long)
+			conn = mysql.connect()
+			cursor = conn.cursor()
+			cursor.execute(sql, data)
+			conn.commit()
+			resp = jsonify('Event added successfully')
+			resp.status_code = 200
+			return resp
 	except Exception as e:
 		print(e)
-	finally:
-		print("nope")
+
+@app.errorhandler(404)
+def not_found(error=None):
+	message = {
+		'status': 404,
+		'message': 'Not Found: ' + request.url,
+	}
+	resp = jsonify(message)
+	resp.status_code = 404
+	return resp
 
 if __name__ == "__main__":
 	app.run()
