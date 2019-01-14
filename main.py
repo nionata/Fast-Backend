52ce15d3a544eefe0bf5ac12da603533ce61bf86
Nicholas Ionata <n.ionata129@gmail.com>
2019-01-14 00:10:42 -0500

    Beefed up sign in endpoint

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 import pymysql
+import time
 from app import app
 from db_config import mysql
 from flask import jsonify
@@ -61,6 +62,8 @@ def add_event():
 			resp = jsonify('Event added successfully')
 			resp.status_code = 200
 			return resp
+		else:
+			return not_found
 	except Exception as e:
 		print(e)
 
@@ -80,6 +83,8 @@ def add_member():
 			resp = jsonify('Member added successfully')
 			resp.status_code = 200
 			return resp
+		else:
+			return not_found
 	except Exception as e:
 		print(e)
 
@@ -89,13 +94,41 @@ def sign_in():
 		_json = request.json
 		_code = _json['event code']
 		_id = _json['member id']
-		if _code and _id:
-			#Some sort of hash for code -> eid
-			#Go to event
-			#Get the type and points associated
-			#Add points to member
+		_lat = _json['lat']
+		_long = _json['long']
+		if _code and _id and _lat and _long:
+			resp = None
+			#(Add hash function code -> eid)
+			eid = _code
+			#Check if member has already signed into event
+			#Get the corresponding event
+			conn = mysql.connect()
+			cursor = conn.cursor(pymysql.cursors.DictCursor)
+			cursor.execute("select event_type_id, event_start, event_end, event_lat, event_long from events where event_id=" + eid)
+			rows = cursor.fetchall()
+			if rows:
+				row = rows[0]
+				#Check if it is within the time range
+				curr = time.time()
+				if row["event_start"] <= curr and curr <= row["event_end"]:
+					#Check location (Add function for proximity)
+					if row["event_lat"] == _lat and row["event_long"] == _long:
+						cursor.execute("update members set member_points=member_points + (select type_points from types where type_id=" + str(row["event_type_id"]) + ") where member_id=" + _id)
+						cursor.execute("insert into attendance (attendance_event_id, attendance_member_id) values (%s, %s)" % (eid, _id))
+						conn.commit()
+						resp = "Successfully signed in"
+					else:
+						resp = "You are not within the range"
+				else:
+					resp = "This event is not active"
+			else:
+				resp = "That is not a valid event code"
 			#Add attendance to table
-			return "ligm"
+			resp = jsonify(resp)
+			resp.status_code = 200
+			return resp
+		else:
+			return not_found
 	except Exception as e:
 		print(e)
 
