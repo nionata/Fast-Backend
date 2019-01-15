a05d5eb11ca0cc68bf54e5a0db0a70e8896eab1d
Nicholas Ionata <n.ionata129@gmail.com>
2019-01-15 14:34:38 -0500

    Added sessions to prevent multiple logins from the same device

--- main.py
+++ main.py
@@ -3,7 +3,7 @@ import time
 from app import app
 from db_config import mysql
 from flask import jsonify
-from flask import flash, request
+from flask import flash, request, session
 
 @app.route('/api/events')
 def get_events():
@@ -101,29 +101,32 @@ def sign_in():
 			#(Add hash function code -> eid)
 			eid = _code
 			#Check if member has already signed into event
-			#Get the corresponding event
-			conn = mysql.connect()
-			cursor = conn.cursor(pymysql.cursors.DictCursor)
-			cursor.execute("select event_type_id, event_start, event_end, event_lat, event_long from events where event_id=" + eid)
-			rows = cursor.fetchall()
-			if rows:
-				row = rows[0]
-				#Check if it is within the time range
-				curr = time.time()
-				if row["event_start"] <= curr and curr <= row["event_end"]:
-					#Check location (Add function for proximity)
-					if row["event_lat"] == _lat and row["event_long"] == _long:
-						cursor.execute("update members set member_points=member_points + (select type_points from types where type_id=" + str(row["event_type_id"]) + ") where member_id=" + _id)
-						cursor.execute("insert into attendance (attendance_event_id, attendance_member_id) values (%s, %s)" % (eid, _id))
-						conn.commit()
-						resp = "Successfully signed in"
+			if not str(eid) in session:
+				#Get the corresponding event
+				conn = mysql.connect()
+				cursor = conn.cursor(pymysql.cursors.DictCursor)
+				cursor.execute("select event_type_id, event_start, event_end, event_lat, event_long from events where event_id=%s" % eid)
+				rows = cursor.fetchall()
+				if rows:
+					row = rows[0]
+					#Check if it is within the time range
+					curr = time.time()
+					if row["event_start"] <= curr and curr <= row["event_end"]:
+						#Check location (Add function for proximity)
+						if row["event_lat"] == _lat and row["event_long"] == _long:
+							cursor.execute("update members set member_points=member_points + (select type_points from types where type_id=%s) where member_id=%s" % (row["event_type_id"], _id))
+							cursor.execute("insert into attendance (attendance_event_id, attendance_member_id) values (%s, %s)" % (eid, _id))
+							conn.commit()
+							resp = "Successfully signed in"
+							session[eid] = ""
+						else:
+							resp = "You are not within the range"
 					else:
-						resp = "You are not within the range"
+						resp = "This event is not active"
 				else:
-					resp = "This event is not active"
+					resp = "That is not a valid event code"
 			else:
-				resp = "That is not a valid event code"
-			#Add attendance to table
+				resp = "You have already signed into this event"
 			resp = jsonify(resp)
 			resp.status_code = 200
 			return resp
