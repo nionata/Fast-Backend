6d880144da7120202375d6365037df9082bf2485
Nicholas Ionata <n.ionata129@gmail.com>
2019-01-24 01:20:40 -0500

    Fixed implementing sign in logic

--- main.py
+++ main.py
@@ -1,13 +1,9 @@
 import pymysql
-import time
 from app import app
 from db_config import mysql
 from flask import jsonify
 from flask import flash, request, session
 from functions import toCode, takeTime
-#from cache import cache
-
-#auth for the admin accs
 
 @app.route('/api/events')
 def get_events():
@@ -35,7 +31,6 @@ def get_members():
 
 @app.route('/api/member/<id>')
 def get_member(id):
-	print(id)
 	try:
 		cursor = mysql.connect().cursor(pymysql.cursors.DictCursor)
 		cursor.execute("SELECT attendance_id, attendance_time_in, event_name, event_start, event_end, type_name FROM attendance inner join events on attendance_event_id=event_id inner join types on event_type_id=type_id where attendance_member_id=%s" % id)
@@ -64,7 +59,7 @@ def add_event():
 		_json = request.json
 		_name = _json['name']
 		_type = _json['type']
-		_start = _json['start'] if _json['start'] else takeTime()
+		_start = _json['start'] if 'start' in _json else takeTime()
 		_end = _json['end']
 		_lat = _json['lat']
 		_long = _json['long']
@@ -118,35 +113,34 @@ def sign_in():
 		_long = _json['long']
 		if _code and _id and _lat and _long:
 			resp = None
-			#Check if member has already signed into event
 			if not str(_code) in session:
-				#Get the event details
 				conn = mysql.connect()
 				cursor = conn.cursor(pymysql.cursors.DictCursor)
-				#Join...grab event details and attedance details eid + mid on attedance
-				cursor.execute("select event_type_id, event_start, event_end, event_lat, event_long from events where event_start%10000=%s" % _code)
+				cursor.execute("SELECT event_id, event_start, event_end, event_lat, event_long, type_points FROM events INNER JOIN types ON event_type_id=type_id WHERE event_start%%10000=%d" % _code)
 				rows = cursor.fetchall()
 				if rows:
-					row = rows[0]
-					#Check if it is within the time range
-					curr = time.time()
-					if row["event_start"] <= curr and curr <= row["event_end"]:
-						#Check location (Add function for proximity)
-						if row["event_lat"] == _lat and row["event_long"] == _long:
-							#as long as there isn't an attedance record
-							cursor.execute("update members set member_points=member_points + (select type_points from types where type_id=%s) where member_id=%s" % (row["event_type_id"], _id))
-							cursor.execute("insert into attendance (attendance_event_id, attendance_member_id) values (%s, %s)" % (eid, _id)) #time
-							conn.commit()
-							resp = "Successfully signed in"
-							session[eid] = ""
+					event = rows[0]
+					currTime = takeTime()
+					if event["event_start"] <= currTime and currTime <= event["event_end"]:
+						if event["event_lat"] == _lat and event["event_long"] == _long:
+							cursor.execute("SELECT attendance_time_in FROM attendance WHERE attendance_event_id=%d AND attendance_member_id=%d" % (event['event_id'], _id))
+							rows = cursor.fetchall()
+							if not rows:
+								cursor.execute("update members set member_points=member_points+%s where member_id=%s" % (event["type_points"], _id))
+								cursor.execute("insert into attendance (attendance_event_id, attendance_member_id, attendance_time_in) values (%d, %d, %d)" % (event['event_id'], _id, currTime))
+								conn.commit()
+								resp = "Successfully signed in"
+								session[str(_code)] = True
+							else:
+								resp = "You already signed into this event"
 						else:
-							resp = "You are not within the range"
+							resp = "You are not within the event range"
 					else:
 						resp = "This event is not active"
 				else:
 					resp = "That is not a valid event code"
 			else:
-				resp = "You have already signed into this event"
+				resp = "You already signed into this event"
 			resp = jsonify(resp)
 			resp.status_code = 200
 			return resp
