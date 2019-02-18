c60a8d5f3e4fa66358ba17fcfd011d52e9abc359
Nicholas Ionata <n.ionata129@gmail.com>
2019-02-18 12:53:04 -0500

    Updated db name

--- db_config.py
+++ db_config.py
@@ -6,6 +6,6 @@ mysql = MySQL()
 # MySQL configurations
 app.config['MYSQL_DATABASE_USER'] = 'fastapp'
 app.config['MYSQL_DATABASE_PASSWORD'] = '2GyXg4juY3gJYWK'
-app.config['MYSQL_DATABASE_DB'] = 'fast'
+app.config['MYSQL_DATABASE_DB'] = 'fastapp$fast'
 app.config['MYSQL_DATABASE_HOST'] = 'fastapp.mysql.pythonanywhere-services.com'
 mysql.init_app(app)
